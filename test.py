e77fc74134d8e4da3b721b4ab364ebff90123169
Nino <nino.zoric@yahoo.de>
2024-06-16 23:44:14 +0200

    Update test.py
    Visualisierung der besten Aktionen angepasst

--- test.py
+++ test.py
@@ -133,36 +133,50 @@ plt.show()
 
 # Optimale Politik visuell darstellen
 def plot_policy(optimal_policy):
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.set_xlim(-1, 4)
-    ax.set_ylim(-1, 2)
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.set_xlim(-0.5, 3.5)
+    ax.set_ylim(-0.5, 1.5)
     ax.invert_yaxis()
 
+    # Neue Raumpositionen als Dictionary (alle auf einer Achse)
     positions = {
         'A': (0, 1), 'B': (0, 0), 'C': (1, 1), 'D': (1, 0), 'E': (2, 1), 'F': (2, 0), 'G': (3, 1)
     }
 
+    # Zeichne Rechtecke und Aktionen
     for room, pos in positions.items():
+        # Zeichne das Rechteck für den Raum
+        rect = plt.Rectangle((pos[0] - 0.5, pos[1] - 0.25), 1, 0.5, edgecolor='black', facecolor='lightgrey', lw=2)
+        ax.add_patch(rect)
+
+        # Hole die beste Aktion für den Raum
         action = optimal_policy[room]
-        ax.text(pos[0], pos[1], f"{room}\n{action}",
-                ha='center', va='center', fontsize=12,
-                bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=1'))
 
+        # Zeichne den Raumname in die Mitte der linken Hälfte des Rechtecks
+        ax.text(pos[0] - 0.25, pos[1], f"{room}",
+                ha='center', va='center', fontsize=12, weight='bold')
 
-        # Zeichne den Pfeil für die Aktion
+        # Zeichne den Pfeil in die Mitte der rechten Hälfte des Rechtecks
+        arrow_length = 0.15
+        arrow_offset = 0.25  # Offset für den Pfeil, damit er in der rechten Hälfte liegt
+
         if action == 'left':
-            ax.arrow(pos[0], pos[1], -0.3, 0, head_width=0.1, head_length=0.1, fc='k', ec='k')
+            ax.arrow(pos[0] + arrow_offset, pos[1], -arrow_length, 0, head_width=0.05, head_length=0.05, fc='k', ec='k')
         elif action == 'right':
-            ax.arrow(pos[0], pos[1], 0.3, 0, head_width=0.1, head_length=0.1, fc='k', ec='k')
+            ax.arrow(pos[0] + arrow_offset, pos[1], arrow_length, 0, head_width=0.05, head_length=0.05, fc='k', ec='k')
         elif action == 'up':
-            ax.arrow(pos[0], pos[1], 0, -0.3, head_width=0.1, head_length=0.1, fc='k', ec='k')
+            ax.arrow(pos[0] + arrow_offset, pos[1], 0, -arrow_length, head_width=0.05, head_length=0.05, fc='k', ec='k')
         elif action == 'down':
-            ax.arrow(pos[0], pos[1], 0, 0.3, head_width=0.1, head_length=0.1, fc='k', ec='k')
+            ax.arrow(pos[0] + arrow_offset, pos[1], 0, arrow_length, head_width=0.05, head_length=0.05, fc='k', ec='k')
 
+    # Setze die Achsen-Ticks und -Labels aus
+    ax.set_xticks(np.arange(4))
+    ax.set_yticks(np.arange(2))
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
 
-    ax.set_xticks([])
-    ax.set_yticks([])
     plt.title('Optimale Politik')
     plt.show()
+
 
 plot_policy(optimal_policy)
